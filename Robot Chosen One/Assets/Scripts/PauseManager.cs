cbe8ef76e7d34b982f81ac3456686bc04e12079c
rodrigueznCDM <rodriguezn@educ.college-montreal.qc.ca>
2026-01-19 14:56:20 -0500

    will resolve player input later

--- Robot Chosen One/Assets/Scripts/PauseManager.cs
+++ Robot Chosen One/Assets/Scripts/PauseManager.cs
@@ -6,11 +6,14 @@ public class PauseManager : MonoBehaviour
 {
     public static bool isPaused = false;
     [SerializeField] GameObject PauseMenu;
+    private PlayerMovement player;
+
 
     private void Start()
     {
         PauseMenu.SetActive(false);
     }
+
 
     public void OnPause(InputValue value)
     {
@@ -18,11 +21,17 @@ public class PauseManager : MonoBehaviour
         {
             PauseGame();
         }
-        else
+    }
+
+
+    public void OnResume(InputValue value)
+    {
+        if (isPaused)
         {
             ResumeGame();
         }
     }
+
 
     public void PauseGame()
     {
@@ -30,7 +39,9 @@ public class PauseManager : MonoBehaviour
         AudioListener.pause = true;
         isPaused = true;
         PauseMenu.SetActive(true);
+        player.SwitchActionMap(1);
     }
+
 
     public void ResumeGame()
     {
@@ -38,5 +49,6 @@ public class PauseManager : MonoBehaviour
         AudioListener.pause = false;
         isPaused = false;
         PauseMenu.SetActive(false);
+        player.SwitchActionMap(0);
     }
 }
