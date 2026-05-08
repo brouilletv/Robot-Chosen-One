e561c56f3b5c4d53c23c29155aa1e1b23f77de9e
KimyCDM <kimy@educ.college-montreal.qc.ca>
2026-05-08 12:06:32 -0400

    Gotta change portal position

---
+++ Robot Chosen One/Assets/Scripts/Systems/Backroom Camera.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackroomCamera : MonoBehaviour
+{
+
+    private Transform CameraT;
+    private Transform player;
+    private PlayerMovement playerMovement;
+
+    public float damping;
+    public float cameraPanOffset;
+    private Vector3 vel = Vector3.zero;
+
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        CameraT = GetComponent<Transform>();
+        player = GameObject.FindWithTag("Player").transform;
+        playerMovement = player.GetComponent<PlayerMovement>();
+        Vector3 RobotP = player.position;
+        RobotP.z = CameraT.position.z;
+        CameraT.position = Vector3.SmoothDamp(CameraT.position, RobotP, ref vel, 0f);
+    }
+
+
+    void Update()
+    {
+        Vector3 RobotP = player.position;
+        RobotP.z = CameraT.position.z;
+
+        RobotP.y += cameraPanOffset * 1;
+
+        CameraT.position = Vector3.SmoothDamp(CameraT.position, RobotP, ref vel, damping);
+    }
+
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
