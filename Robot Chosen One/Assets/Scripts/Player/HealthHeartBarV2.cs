7348297aebb1a4b951051bb28f50d22b8216a1e2
brouilletv <brouilletv@educ.college-montreal.qc.ca>
2026-05-09 21:02:58 -0400

    they need to nerf coding bugs

--- Robot Chosen One/Assets/Scripts/Player/HealthHeartBarV2.cs
+++ Robot Chosen One/Assets/Scripts/Player/HealthHeartBarV2.cs
@@ -70,11 +70,22 @@ public class HealthHeartBarV2 : MonoBehaviour
 
     public void TakeDamage(float amount)
     {
-        SetHealth(health - amount);
+        if (imunity is false)
+        {
+            StartCoroutine(ImunityCooldown());
+            SetHealth(health - amount);
+        }
         if (health <= 0)
         {
             Dead(respawnTime);
             StartCoroutine(DeathHealCooldown());
+        }
+
+        IEnumerator ImunityCooldown()
+        {
+            imunity = true;
+            yield return new WaitForSeconds(imunityTime);
+            imunity = false;
         }
     }
 
@@ -111,18 +122,6 @@ public class HealthHeartBarV2 : MonoBehaviour
         if (amount > 0)
         {
             SetHealth(health + amount);
-        }
-        else if (imunity is false)
-        {
-            StartCoroutine(ImunityCooldown());
-            SetHealth(health + amount);
-        }
-
-        IEnumerator ImunityCooldown()
-        {
-            imunity = true;
-            yield return new WaitForSeconds(imunityTime);
-            imunity = false;
         }
     }
 
